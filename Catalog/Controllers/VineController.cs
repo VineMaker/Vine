053122ac90ac9318564ad169445066687725bed7
ssarakatch <ssarakatch@gmail.com>
2012-09-01 15:20:06 +0300

    added vine controller

---
+++ Catalog/Controllers/VineController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Catalog.Controllers
+{
+    public class VineController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Vine controller";
+
+            return View();
+        }
+
+        public ActionResult GetAll()
+        {
+            return View();
+        }
+    }
+}
